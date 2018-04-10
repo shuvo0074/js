198679edf6d79cb0433657b2fdbc06d80b75dd15
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2018-04-10 14:42:22 +0600

    Shallow copy and deep copy

--- test.js
+++ test.js
@@ -21,8 +21,15 @@ console.log(planets)
 var morePlanets = ["pluto","mars","asdf","qwert"]
 console.log("**slice " + planets.slice(-3)) //Accessing last 3 elements
 //Slice doesn't delete the selected chunk, splice does
-var pl=morePlanets // A deep copy hppened here where the array pl is copied from the memory where morePlanets is saved. 
+var pl=morePlanets // A deep copy hppened here where the array pl is copied from the memory where morePlanets is saved.
 morePlanets[1]="*****"
-console.log("this is pl " + pl)
-//To do shallow copy , where an array is copied from the contents of another array, we can use slice method. when we copy a variable, shallow copy automatically happens
+console.log("this is pl-- " + pl + " -- this is morePlanets -- " + morePlanets , "\nDeep copy" , "...\n\n")
+/*To do shallow copy , where an array is copied from the contents of another array, we can use slice method. when we copy a variable, shallow copy automatically happens
+In deep copy, the array itself is copied from memory instance.
+In shallow copy, the values inside the array is copied to the other array.
+shallow copy can also be done like this (in CS6)-- pl=[...morePlanets]
+*/
+pl = [...morePlanets]
+pl[1]="----"
+console.log("this is pl -- " + pl + " -- this is morePlanets -- " + morePlanets , "\nShallow copy" , "...\n\n")
 /*console.log(planets.reverse())*/
