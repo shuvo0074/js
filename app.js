6cb99a667526f0a3acfb3e6dfa174b643b78ee93
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2018-04-10 17:01:28 +0600

    Import single function or object in the modules

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ function helloWorld (name) {
     console.log("Hello " + name)
 }
 helloWorld('ERA')
+//To load a module we need the "require" function in the main module
+const logger = require('./logger') // We save the imported module inside a constant named logger
+//console.log("--*- " + logger) // To see what is inside the imported module
+
+logger('any message') //This is when we import only a function of the object
+//logger.log('a message') //This is when we import the whole objectas a module
---
+++ logger.js
@@ -0,0 +1,9 @@
+var url = 'http://abcd.com\log'
+//export const adrs = url;
+
+function log(message) {
+    //Send an http request
+    console.log(message)
+}
+//module.exports.log = log; //This is when we export the whole object
+module.exports = log; //This is when we export only a function of the object
