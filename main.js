5057c9633aa2fba361937b48baedb0ec4c1cd4fd
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2018-04-15 15:27:49 +0600

    event , local/global variable

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const newLocal = "The most unnecessary button is clicked";
+var c=10;
+document.write(c , " **\n")
+//console.log(c+ " **")
 function runCode(){
-    document.write("Hello world")
-    console.log(newLocal)
+    document.write("Hello world, " + newLocal +" ")
+
+    //console.log(newLocal)
     alert("Clicked")
 }
